7441b00565d8f666eb5bd4a07c394382943122e7
Radek Drlik <radek.drlik@gmail.com>
2013-07-25 20:49:18 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(reques 2t, response) {
-  response.send('Hello World 2 !');
+app.get('/', function(request, response) {
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
