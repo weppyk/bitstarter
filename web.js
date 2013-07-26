6f34ff7014bb595e3dd2f598994bfd6e9e3ffed9
Radek Drlik <radek.drlik@gmail.com>
2013-07-26 13:18:06 +0000

    link to index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+var htmlfile = fs.readFileSync('index.html','utf8')
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(htmlfile);
+
+//gg
 });
 
 var port = process.env.PORT || 5000;
